e5f422f7af1ff27b299d99f3410a40c13f65b87a
wolpis <star@devksy.xyz>
2024-08-25 08:21:07 +0900

    update(searchpage)

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,18 @@
-import { HeadersComponent, Ranking } from "./component"
+import { useSearchParams } from "react-router-dom"
+import { HeadersComponent, Ranking, SearchResult } from "./component"
 import style from "./style.module.css"
 
 const SearchPage = () => {
+    const [params, SetParams] = useSearchParams();
+    const search_txt = params.get("q")
+
     return (
         <div className={style.main__init__}>
             <div className={style.main_container}>
-                <HeadersComponent />
-                <Ranking />
+                {
+                    search_txt === null ? <><HeadersComponent /><Ranking /></> : <><HeadersComponent /><SearchResult /></>
+                }
+
             </div>
         </div>
     )
