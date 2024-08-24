3741b8dcf6e74b6d5a656f30990461544f8e0050
Ushio-Hayase <UshioHayase@gmail.com>
2024-08-24 17:58:37 +0900

    search bar

--- src/pages/main/component.tsx
+++ src/pages/main/component.tsx
@@ -1,4 +1,5 @@
-
+import { CiLocationOn } from "react-icons/ci";
+import styles from "./style.module.css";
 
 export const MainHeader = () => {
     return (
@@ -10,8 +11,16 @@ export const MainHeader = () => {
 
 export const SearchBar = () => {
     return (
-        <div>
-
+        <div className={styles.searchbar}>
+            <div className={styles.outer_left_searchbar}>
+                <img src="/Search.svg" className={styles.inner_searchbar_image} />
+                <input type="text" className={styles.inner_left_searchbar}/>
+            </div>
+            <div className={styles.outer_center_searchbar}></div>
+            <CiLocationOn className={styles.inner_right_image}/>
+            <div className={styles.outer_right_searchbar}>
+                <div className={styles.inner_right_searchbar}>Tower 730</div>
+            </div>
         </div>
     )
 }
