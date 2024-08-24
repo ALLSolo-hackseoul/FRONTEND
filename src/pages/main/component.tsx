b3c9ede578b266fa87a8feddd44c6ed0436ae01e
wolpis <star@devksy.xyz>
2024-08-24 18:04:52 +0900

    bugfix(index)

--- src/pages/main/component.tsx
+++ src/pages/main/component.tsx
@@ -1,4 +1,4 @@
-import styles from "./style.module.css";
+import style from "./style.module.css";
 import { CiLocationOn } from "react-icons/ci";
 import { LuBadgePercent, LuShoppingCart } from "react-icons/lu";
 import { useNavigate, useSearchParams } from "react-router-dom";
@@ -19,15 +19,15 @@ export const MainHeader = () => {
 
 export const SearchBar = () => {
     return (
-        <div className={styles.searchbar}>
-            <div className={styles.outer_left_searchbar}>
-                <img src="/Search.svg" className={styles.inner_searchbar_image} />
-                <input type="text" className={styles.inner_left_searchbar}/>
+        <div className={style.searchbar}>
+            <div className={style.outer_left_searchbar}>
+                <img src="/Search.svg" className={style.inner_searchbar_image} />
+                <input type="text" className={style.inner_left_searchbar} />
             </div>
-            <div className={styles.outer_center_searchbar}></div>
-            <CiLocationOn className={styles.inner_right_image}/>
-            <div className={styles.outer_right_searchbar}>
-                <div className={styles.inner_right_searchbar}>Tower 730</div>
+            <div className={style.outer_center_searchbar}></div>
+            <CiLocationOn className={style.inner_right_image} />
+            <div className={style.outer_right_searchbar}>
+                <div className={style.inner_right_searchbar}>Tower 730</div>
             </div>
         </div>
     )
