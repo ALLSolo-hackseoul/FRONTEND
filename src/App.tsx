597cea36fdce994e8125f8a508135cf6cc5c49d2
wolpis <star@devksy.xyz>
2024-08-25 10:43:11 +0900

    update(aboutpage)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import MainPage from './pages/main'
 import SearchPage from './pages/search'
-<<<<<<< HEAD
 import AuthPage from './pages/auth'
-=======
 import AboutPage from './pages/about'
->>>>>>> 11d34fbe9febf245e0ce4a56bf9f3ed16df77f69
 
 function App() {
 
@@ -14,11 +11,8 @@ function App() {
     <Routes>
       <Route path='/' element={<MainPage />} />
       <Route path='/search' element={<SearchPage />} />
-<<<<<<< HEAD
       <Route path='/auth' element={<AuthPage />} />
-=======
       <Route path='/about/:id' element={<AboutPage />} />
->>>>>>> 11d34fbe9febf245e0ce4a56bf9f3ed16df77f69
     </Routes>
   )
 }
